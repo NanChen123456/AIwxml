5c58981e43d8d3a8fb54501f2cf0d3045a26581d
NanChen123456 <149640725+NanChen123456@users.noreply.github.com>
2026-03-18 14:47:47 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+﻿const CLOUD_ENV_ID = 'cloud1-7gqhrdyj857f5c4c'
+
+App({
+  onLaunch() {
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 及以上基础库以支持云能力')
+      return
+    }
+
+    wx.cloud.init({
+      env: CLOUD_ENV_ID,
+      traceUser: true,
+    })
+
+    this.globalData.cloudEnvId = CLOUD_ENV_ID
+  },
+
+  globalData: {
+    cloudEnvId: CLOUD_ENV_ID,
+  },
+})
